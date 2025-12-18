0b6b4dacfa3456c4e42d9b52a47ad5415dbf0dce
cyroz1 <cyrz@pm.me>
2025-12-18 15:33:57 -0500

    final test

--- platforms/linux/fedora/vidcord.spec
+++ platforms/linux/fedora/vidcord.spec
@@ -1,5 +1,5 @@
 Name:           vidcord
-Version:        5.0
+Version:        5.1
 Release:        1%{?dist}
 Summary:        Video Compressor
 
--- vidcord.py
+++ vidcord.py
@@ -22,7 +22,7 @@ from qfluentwidgets import (FluentWindow, NavigationItemPosition, FluentIcon as 
                             InfoBar, InfoBarPosition, Theme, setTheme, CardWidget,
                             SimpleCardWidget, ImageLabel, ScrollArea, HyperlinkButton)
 
-CURRENT_VERSION = "v5.0"
+CURRENT_VERSION = "v5.1"
 
 # --- LOGGING SETUP ---
 log_path = os.path.join(os.path.expanduser('~'), 'vidcord_crash.log')
